f27f723f077d1980e4c565d4938b2bcf06ffb254
Jazmin M <jazminmerckel@gmail.com>
2019-09-28 02:06:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,8 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
-  else
-    return
   end
 end
