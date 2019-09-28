ec93ebc01edba3d25b8a41f229285d981899b6d5
Jazmin M <jazminmerckel@gmail.com>
2019-09-28 01:58:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array == 
+  if array.length == 1
     return array.join
-  elsif array ==
+  elsif array.length == 2
     return array.join("and")
-  elsif array ==
+  elsif array.length == 3
     return array.join(",")
   else
     return
