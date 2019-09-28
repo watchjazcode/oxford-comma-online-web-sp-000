d83093b3bc1d5fdc86764e098e1cad2e5ebbe388
Jazmin M <jazminmerckel@gmail.com>
2019-09-28 01:56:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
   if array == 
+    return array.join
+  elsif array ==
+    return array.join("and")
+  elsif array ==
+    return array.join(",")
+  else
+    return
+  end
 end
